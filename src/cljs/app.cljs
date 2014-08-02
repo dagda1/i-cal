0d48d48e73e614b76d61897ca1ab533b9afe65fe
dagda1 <dagda1@scotalt.net>
2014-08-02 21:27:35 +0100

    separate ical component

--- src/cljs/app.cljs
+++ src/cljs/app.cljs
@@ -3,22 +3,12 @@
     [figwheel.client :as fw :include-macros true]
     [om.core :as om :include-macros true]
     [om.dom :as dom :include-macros true]
+    [ical.core :as ical]
     [sablono.core :as html :refer-macros [html]]))
-
-(def state (atom {:doc {} :saved? false}))
 
 (enable-console-print!)
 
-(defn log [s]
-  (.log js/console (str s)))
-
-(log "hoors droop")
-
-(defn widget [data]
-  (om/component
-   (html [:h2 "This is an om component, " (:text data)])))
-
-(om/root widget {:text "hoors droop"} {:target js/document.body})
+(om/root ical/ical {} {:target js/document.body})
 
 (fw/watch-and-reload
   ;; :websocket-url "ws://localhost:3449/figwheel-ws" default
---
+++ src/cljs/ical/core.cljs
@@ -0,0 +1,16 @@
+(ns ical.core
+  (:require
+    [om.core :as om :include-macros true]
+    [om.dom :as dom :include-macros true]
+    [sablono.core :as html :refer-macros [html]]))
+
+(enable-console-print!)
+
+(defn log [s]
+  (.log js/console (str s)))
+
+(log "floors or doors")
+
+(defn ical [data]
+  (om/component
+   (html [:h2 "This will be the calendar, so it will"])))
