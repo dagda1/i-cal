02bdf632493c34c66fc560c43615078a5ed441e1
dagda1 <dagda1@scotalt.net>
2014-08-02 23:02:47 +0100

    calendar

--- src/cljs/ical/core.cljs
+++ src/cljs/ical/core.cljs
@@ -6,9 +6,22 @@
 
 (enable-console-print!)
 
+(def weekdays ["saturday" "monday" "tuesday" "wednesday" "thursday" "friday" "sunday"])
+
 (defn log [s]
   (.log js/console (str s)))
 
 (defn ical [data]
-  (om/component
-   (html [:h2 "Render calendar"])))
+  (reify
+    om/IDisplayName
+      (display-name [_]
+        (or (:react-name opts) "calendar"))
+    om/IRender
+      (render [this]
+        (html/html [:div.calendar-toolbar
+                    [:div.btn-group.pull-right
+                     [:a.right {:href "#"} "Right"]
+                     [:a.left {:href "#"} "Left"]]
+                    [:h3 "August 2014"]]))))
+
+
