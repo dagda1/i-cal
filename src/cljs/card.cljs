d6bec05d581d32be8622ce5a71b32ad0d864bda2
dagda1 <dagda1@scotalt.net>
2014-08-02 21:51:27 +0100

    devcard setup

--- src/cljs/card.cljs
+++ src/cljs/card.cljs
@@ -14,17 +14,5 @@
 
 (dc/start-figwheel-reloader!)
 
-(defonce om-test-atom (atom {:count 20}))
-
-(defn counter [owner data f s]
-  (om/component
-   (sab/html
-    [:div
-     [:h1 (om/get-shared owner :title) (:count data)]
-     [:div [:a {:onClick #(om/transact! data :count f)} s]]
-     (dc/edn->html data)])))
-
-(defn om-counter-inc [data owner] (counter owner data inc "inc"))
-
-(defcard omcard-shared-ex-1
-  (dc/om-root-card om-counter-inc om-test-atom {:shared {:title "First counter "}}))
+(defcard omcard-ical
+  (dc/om-root-card ical/ical {}))
