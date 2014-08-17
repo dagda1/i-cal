f1669b7fda5e2a0f1c3c659d07ed25497ddfa08e
dagda1 <dagda1@scotalt.net>
2014-08-17 17:06:46 +0100

    render calendar

--- src/cljs/ical/core.cljs
+++ src/cljs/ical/core.cljs
@@ -6,14 +6,11 @@
 
 (enable-console-print!)
 
-(def date-format "DD/MM/YYYY")
-
 (def weekdays ["saturday" "monday" "tuesday" "wednesday" "thursday" "friday" "sunday"])
 
 (defn log [s]
   (.log js/console (str s)))
 
-; moment().add('month', -1).endOf('month').startOf('week').add('days',1).format()
 (defn last-monday [current]
   (->
     current
@@ -21,15 +18,6 @@
     (.endOf "month")
     (.startOf "week")
     (.add "days" 1)))
-
-; moment().add("month", 1).startOf('month').endOf('week').add('days', 1).format("dddd/DD/MM/YYYY")
-(defn first-sunday [current]
-  (->
-     current
-     (.add "month" 1)
-     (.startOf "month")
-     (.endOf "week")
-     (.add "days" 1)))
 
 (defn first-day-of-month [current]
   (let [cloned-date (.clone current)
@@ -39,27 +27,18 @@
       start-of-month
       (last-monday cloned-date))))
 
-(defn last-day-of-month [current]
-  (let [cloned-date (.clone current)
-        end-of-month (->
-                        cloned-date
-                        (.endOf "month"))
-        week-day (.weekday end-of-month)]
-   (if (= 0 week-day)
-     end-of-month
-     (first-sunday end-of-month))))
-
-(defn weeks [start-date]
-  (vec (for [week-counter (range 0 35 7)]
-    (vec (for [day-counter (range 7)]
-      (->
-       start-date
+(defn render-days [week-counter day-counter]
+  (let [first-day-of-month (first-day-of-month (js/moment (new js/Date)))]
+    [:td
+     (->
+       first-day-of-month
        (.clone)
-       (.add "days" (+ week-counter day-counter))))))))
+       (.add "days" (+ week-counter day-counter))
+       (.format "DD"))]))
 
 (defn render-weeks [week-counter]
   [:tr
-   [:td "flappies"]])
+   (map (partial render-days week-counter) (range 7))])
 
 (defn ical [data owner]
   (reify
@@ -68,17 +47,13 @@
         (or (:react-name opts) "calendar"))
     om/IRender
       (render [this]
-        (let [today (js/moment (new js/Date))
-              first-day-of-month (first-day-of-month today)
-              weeks (weeks first-day-of-month)]
-          (log weeks)
           (html/html [:div.calendar-toolbar
-                      [:div.btn-group.pull-right
-                       [:a.right {:href "#"} "Right"]
-                       [:a.left {:href "#"} "Left"]]
-                      [:h3 "August 2014"]
-                      [:table.table.table-striped.table-bordered.table-hover
-                       [:thead
-                        [:tr (map #(identity [:th %]) weekdays)]]
-                       [:tbody
-                        (map render-weeks (range 0 35 7))]]])))))
+            [:div.btn-group.pull-right
+             [:a.right {:href "#"} "Right"]
+             [:a.left {:href "#"} "Left"]]
+            [:h3 "August 2014"]
+            [:table.table.table-striped.table-bordered.table-hover
+             [:thead
+              [:tr (map #(identity [:th %]) weekdays)]]
+             [:tbody
+              (map render-weeks (range 0 35 7))]]]))))
